ce9eaad3e56a286f29b0f74f7feef04aeb19e44e
priel <prielsasson@gmail.com>
2025-06-25 19:31:29 +0300

    oops can spam button

--- ui.gd
+++ ui.gd
@@ -1,9 +1,13 @@
 extends CanvasLayer
 
+var active := 0
 
 func _on_road_pressed() -> void:
 	if %Resources.brick > 0 and %Resources.wood > 0:
+		active += 1
+		var my = active
 		Globals.build_road.emit()
 		await Globals.built
-		%Resources.brick -= 1
-		%Resources.wood -= 1
+		if my == active:
+			%Resources.brick -= 1
+			%Resources.wood -= 1
