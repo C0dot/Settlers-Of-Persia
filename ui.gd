68546b03451eea687e21eaf1ecb12820f3c71175
priel <prielsasson@gmail.com>
2025-06-25 19:24:39 +0300

    minor fix

--- ui.gd
+++ ui.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 
 func _on_road_pressed() -> void:
 	if %Resources.brick > 0 and %Resources.wood > 0:
+		Globals.build_road.emit()
+		await Globals.built
 		%Resources.brick -= 1
 		%Resources.wood -= 1
-		Globals.build_road.emit()
